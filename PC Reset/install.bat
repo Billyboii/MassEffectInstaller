dad250fa8ead7140e1d327f3edf3db3cb774a91c
Billyboii <billyboi456@gmail.com>
2024-06-02 13:11:52 -0400

    added new code and some start to powershell file (#1)

---
+++ PC Reset/install.bat
@@ -0,0 +1,6 @@
+for %%i in (%cd%\*.exe) do %%i
+for %%i in (%cd%\*.msi) do %%i
+"%cd%\1\setup.exe"
+"%cd%\2\setup.exe"
+"%cd%\3\setup.exe"
+"%cd%\4\setup.exe"
--- download.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-bash -c "curl -# -o %fname%.tmp "%URL%""
-tasklist /FI "IMAGENAME eq bash.exe" 2>NUL | find /I /N "bash.exe">NUL 
-if errorlevel 0 (
-rename %fname%.tmp %fname%%fnameext%
-exit
-)
